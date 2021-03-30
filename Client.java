9f0c04614c106d5ec19f8289fc5b5005253fd9cf
Zeftali <80684834+Zeftali@users.noreply.github.com>
2021-03-30 12:52:50 +1100

    Update Client.java
    This is what I have been working on on Visual Studio Code check it out, we need to do the JOBN on line 84 to schedule the jobs

--- Client.java
+++ Client.java
@@ -1,55 +1,250 @@
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.Socket;
+import java.net.*;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
-import javax.lang.model.element.Element;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+
+import java.io.*;
 
 public class Client {
-  private Socket socket;
-  private BufferedReader input;
-  private DataOutputStream out;
-  private BufferedReader in;
-  static private ArrayList<Element> serverType;
-  static private ArrayList<Server> servers;
-  private DataInputStream DIS;
-  private DataOutputStream DOUT;
+	// initialize socket and input output streams
+	private static Socket socket = null;
+	private BufferedReader input = null;
+	private DataOutputStream out = null;
+	private BufferedReader in = null;
+	
 
+	// constructor to put ip address and port
+	public Client(String address, int port) throws IOException {
 
-  public Client(String Ipaddress, int Port, String sorter) throws Exception{
+		// connect to server
+		connect(address, port);
 
-      this.socket = new Socket(Ipaddress, Port); // Used to establish the connection
-      this.input = new BufferedReader(new InputStreamReader(System.in)); // DONT THINK WE NEED THIS
-      this.out = new DataOutputStream(socket.getOutputStream()); // DONT THINK WE NEED THIS
-      DIS = new DataInputStream(socket.getInputStream());
-      DOUT = new DataOutputStream(socket.getOutputStream());
+		// takes input from keyboard
+		input = new BufferedReader(new InputStreamReader(System.in));
 
+		// sends output to the server
+		out = new DataOutputStream(socket.getOutputStream());
 
-  public static void main(String args[]) {
-    String sorter = "";
-    if (args.length == 2) {
-      sorter = args[1];
-    } else {
-      sorter = "ATL";
+		// gets input from server
+		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	}
+
+	private void start () {
+		// keep track of if we are connected to server or not
+		boolean connected = false;
+
+		// client input
+		String outStr = "";
+
+		// Initial handshake
+		sendMessage("HELO");
+		readMessage();
+
+		// Send user details
+		sendMessage("AUTH user");
+		readMessage();
+
+		// read ds-system xml file and populate Server ArrayList
+		ArrayList<Server> t = new ArrayList<Server>();
+		t = readXML("ds-system.xml");
+
+		int largest = 0;
+		
+		for (int i = 0; i < t.size(); i++){
+			if (t.get(i).getCores() > largest){
+				largest = i;
+			}
+		}
+
+		// 
+		System.out.println("[Largest server: " + t.get(largest).getType() + " " + t.get(largest).getCores()+"]");
+		
+		// Hand-shake completed - client now connected
+		connected = true;
+
+		// Tells client it is ready to recieve commands
+		sendMessage("REDY");
+
+		// temp string to hold readMessage data
+		// we check the contents of this string, rather than call readMessage() 
+		String msg = readMessage();
+		
+		// if client receive's NONE; send quit and set connected to false
+		// otherwise, we receive JOBN and continue with program
+		if (msg.contains("NONE")) { 
+			sendMessage("QUIT");
+			connected = false;
+		} else if (msg.contains("JOBN ")){
+			
+			/* 	TODO
+			*/
+
+			int jobNo = 0;
+			while (!msg.contains("NONE")){
+				
+				if (msg.contains("JCPL")){
+					sendMessage("REDY");
+					msg = readMessage();
+				} else {
+					sendMessage("SCHD " + jobNo + " " + t.get(largest).getType() + " " + (t.get(largest).getLimit()-1));
+					msg = readMessage();
+
+					sendMessage("REDY");
+					msg = readMessage();
+
+					jobNo ++;
+				}
+
+				
+			}
+			
+			// the code above works up until JOB #6, as it sends us JCPL rather than JOBN...
+		}
+
+		
+		
+		// allow user to input messages until 'QUIT' is sent
+		while (connected){
+			
+			try {
+				outStr = input.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			if (outStr.equals("QUIT")){
+				connected = false;
+				sendMessage("QUIT");
+				break;
+			}
+
+			// send first, THEN read messages
+			sendMessage(outStr);
+			readMessage();
+
+		}
+
+		// close the connection
+		try {
+
+			// QUIT hand-shake, must receive confirmation from server for quit
+			if (readMessage().contains("QUIT")){
+				input.close();
+				out.close();
+				socket.close();
+			}
+				
+			
+		} catch (IOException i) {
+			System.out.println(i);
+		}
+
+		// Exit the program
+		System.exit(1);
+	}
+
+	private void sendMessage (String outStr) {
+		// send message to server
+		byte[] byteMsg = outStr.getBytes();
+		try {
+			out.write(byteMsg);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// Display outgoing message from client
+		System.out.println("OUT: " + outStr);
+	}
+
+	private String readMessage () {
+		// read string sent from server
+		String inStr = "";
+		char[] cbuf = new char[65535];
+		try {
+			in.read(cbuf);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		inStr = new String(cbuf, 0, cbuf.length);
+
+		// Display incoming message from server
+		System.out.println("INC: " + inStr);
+
+		return inStr;
+	}
+
+	private static void connect(String address, int port) {
+		// try establish a connection
+		double secondsToWait = 1;
+		int tryNum = 1;
+		while (true) {
+			try {
+				System.out.println("Connecting to server at: " + address + ":" + port);
+				socket = new Socket(address, port);
+				System.out.println("Connected");
+				break; // we connected, so exit the loop
+			} catch (IOException e) {
+				// reconnect failed, wait.
+				secondsToWait = Math.min(30, Math.pow(2, tryNum));
+				tryNum++;
+				System.out.println("Connection timed out, retrying in  " + (int) secondsToWait + " seconds ...");
+				try {
+					TimeUnit.SECONDS.sleep((long) secondsToWait);
+				} catch (InterruptedException ie) {
+					// interrupted
+				}
+			}
+		}
+	}
+
+	public static void main(String args[]) throws IOException {
+		Client client = new Client("127.0.0.1", 50000);
+		client.start();
+	}
+
+	public static ArrayList<Server> readXML(String fileName){
+        ArrayList<Server> serverList = new ArrayList<Server>();
+		
+		try {
+			// XML file to read
+			File systemXML = new File(fileName);
+
+			// Setup XML document parser
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(systemXML);
+
+			doc.getDocumentElement().normalize();
+			NodeList servers = doc.getElementsByTagName("server");
+			for (int i = 0; i < servers.getLength(); i++) {
+				Element server = (Element) servers.item(i);
+
+				// Parse all XML attributes to new Server object
+				String type = server.getAttribute("type");
+				int limit = Integer.parseInt(server.getAttribute("limit"));
+				int bootupTime = Integer.parseInt(server.getAttribute("bootupTime"));
+				float hourlyRate = Float.parseFloat(server.getAttribute("hourlyRate"));
+				int coreCount = Integer.parseInt(server.getAttribute("coreCount"));
+				int memory = Integer.parseInt(server.getAttribute("memory"));
+				int disk = Integer.parseInt(server.getAttribute("disk"));
+				
+				Server s = new Server(type,limit,bootupTime,hourlyRate,coreCount,memory,disk);
+				serverList.add(s);
+			}
+
+			
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return serverList;
     }
-    Client client = new Client("127.0.0.1", 50000, sorter);
 
-    While(!EOF)   // Pseudocode
-    { 
-      Read(client.DIS);
-    }
-    
-  }
-
-  private static void Read(DataInputStream D) throws Exception
-  {
-      byte[] readingArray = new byte[D.available()];
-      D.read(readingArray);
-      String Message = new String(readingArray);
-  }
+	
 }
