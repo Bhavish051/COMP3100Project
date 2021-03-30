dd1ea890f7d463be64d62da635de2651369a0894
Zeftali <80684834+Zeftali@users.noreply.github.com>
2021-03-30 13:00:14 +1100

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,57 +1,35 @@
 import java.net.*;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
-
-
 import java.io.*;
-
 public class Client {
 	// initialize socket and input output streams
 	private static Socket socket = null;
 	private BufferedReader input = null;
 	private DataOutputStream out = null;
 	private BufferedReader in = null;
-	
-
 	// constructor to put ip address and port
 	public Client(String address, int port) throws IOException {
-
-		// connect to server
 		connect(address, port);
-
-		// takes input from keyboard
 		input = new BufferedReader(new InputStreamReader(System.in));
-
-		// sends output to the server
 		out = new DataOutputStream(socket.getOutputStream());
-
-		// gets input from server
 		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 	}
-
 	private void start () {
-		// keep track of if we are connected to server or not
 		boolean connected = false;
-
-		// client input
 		String outStr = "";
-
-		// Initial handshake
+		
 		sendMessage("HELO");
 		readMessage();
 
-		// Send user details
 		sendMessage("AUTH user");
 		readMessage();
 
-		// read ds-system xml file and populate Server ArrayList
 		ArrayList<Server> t = new ArrayList<Server>();
 		t = readXML("ds-system.xml");
 
@@ -63,21 +41,15 @@ public class Client {
 			}
 		}
 
-		// 
+		// Prints largest server 
 		System.out.println("[Largest server: " + t.get(largest).getType() + " " + t.get(largest).getCores()+"]");
 		
-		// Hand-shake completed - client now connected
 		connected = true;
 
-		// Tells client it is ready to recieve commands
 		sendMessage("REDY");
 
-		// temp string to hold readMessage data
-		// we check the contents of this string, rather than call readMessage() 
 		String msg = readMessage();
-		
-		// if client receive's NONE; send quit and set connected to false
-		// otherwise, we receive JOBN and continue with program
+
 		if (msg.contains("NONE")) { 
 			sendMessage("QUIT");
 			connected = false;
@@ -88,7 +60,6 @@ public class Client {
 
 			int jobNo = 0;
 			while (!msg.contains("NONE")){
-				
 				if (msg.contains("JCPL")){
 					sendMessage("REDY");
 					msg = readMessage();
@@ -100,19 +71,12 @@ public class Client {
 					msg = readMessage();
 
 					jobNo ++;
-				}
-
-				
+				}	
 			}
 			
-			// the code above works up until JOB #6, as it sends us JCPL rather than JOBN...
+			// the code above works up until JOB #6, as it sends us JCPL rather than JOBN
 		}
-
-		
-		
-		// allow user to input messages until 'QUIT' is sent
 		while (connected){
-			
 			try {
 				outStr = input.readLine();
 			} catch (IOException e) {
@@ -125,33 +89,22 @@ public class Client {
 				break;
 			}
 
-			// send first, THEN read messages
 			sendMessage(outStr);
 			readMessage();
-
 		}
-
-		// close the connection
 		try {
-
-			// QUIT hand-shake, must receive confirmation from server for quit
 			if (readMessage().contains("QUIT")){
 				input.close();
 				out.close();
 				socket.close();
-			}
-				
-			
+			}	
 		} catch (IOException i) {
 			System.out.println(i);
 		}
-
-		// Exit the program
 		System.exit(1);
 	}
 
 	private void sendMessage (String outStr) {
-		// send message to server
 		byte[] byteMsg = outStr.getBytes();
 		try {
 			out.write(byteMsg);
@@ -164,7 +117,6 @@ public class Client {
 	}
 
 	private String readMessage () {
-		// read string sent from server
 		String inStr = "";
 		char[] cbuf = new char[65535];
 		try {
@@ -181,7 +133,6 @@ public class Client {
 	}
 
 	private static void connect(String address, int port) {
-		// try establish a connection
 		double secondsToWait = 1;
 		int tryNum = 1;
 		while (true) {
@@ -189,16 +140,14 @@ public class Client {
 				System.out.println("Connecting to server at: " + address + ":" + port);
 				socket = new Socket(address, port);
 				System.out.println("Connected");
-				break; // we connected, so exit the loop
+				break; 
 			} catch (IOException e) {
-				// reconnect failed, wait.
 				secondsToWait = Math.min(30, Math.pow(2, tryNum));
 				tryNum++;
 				System.out.println("Connection timed out, retrying in  " + (int) secondsToWait + " seconds ...");
 				try {
 					TimeUnit.SECONDS.sleep((long) secondsToWait);
 				} catch (InterruptedException ie) {
-					// interrupted
 				}
 			}
 		}
@@ -226,7 +175,6 @@ public class Client {
 			for (int i = 0; i < servers.getLength(); i++) {
 				Element server = (Element) servers.item(i);
 
-				// Parse all XML attributes to new Server object
 				String type = server.getAttribute("type");
 				int limit = Integer.parseInt(server.getAttribute("limit"));
 				int bootupTime = Integer.parseInt(server.getAttribute("bootupTime"));
@@ -243,7 +191,7 @@ public class Client {
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
-		return serverList;
+	return serverList;
     }
 
 	
